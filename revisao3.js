1b454232c90014b2e36fb89fa08b4d59b9a71a11
Eduardo Conde Pires <eduardo.pires@threeo.com.br>
2023-02-20 20:15:05 -0300

    Revisão #03

---
+++ revisao3.js
@@ -0,0 +1,27 @@
+// ES8: Object.values/Object.entries
+const obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+// melhorias na notação literal
+const nome = 'Carla'
+const pessoa = {
+    nome,
+    ola() {
+        return 'Olá'
+    }
+}
+console.log(pessoa.ola(), pessoa.nome)
+
+// class
+class Animal { }
+class Cachorro extends Animal {
+    falar() {
+        return 'au au'
+    }
+}
+console.log(new Cachorro().falar())
