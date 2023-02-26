78a97a3d70db630055b64cff1f44e7a8732fbdc5
Eduardo Conde Pires <eduardo.pires@threeo.com.br>
2023-02-26 11:53:43 -0300

    Async/Await

---
+++ asyncAwait.js
@@ -0,0 +1,39 @@
+// com promises
+const http = require('http')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+
+            res.on('data', dados => {
+                resultado += dados
+            })
+
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch (e) {
+                    console.log(e)
+                }
+            })
+        })
+    })
+}
+
+// recurso do ES8
+// o objetivo é simplificar a utilização de promises
+let obterAlunos = async () => {
+    const tA = await getTurma('A')
+    const tB = await getTurma('B')
+    const tC = await getTurma('C')
+
+    return [].concat(tA, tB, tC)
+} // retorna um objeto AsyncFunction
+
+obterAlunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then(nomes => console.log(nomes))
+
